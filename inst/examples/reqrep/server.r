9392db87a9aa66830aee6ba81d633b661601a953
wrathematics <wrathematics@gmail.com>
2015-10-01 08:08:44 -0400

    moved reqrep example, added some warnings/errors

--- inst/examples/reqrep/server.r
+++ inst/examples/reqrep/server.r
@@ -1,3 +1,6 @@
+if (interactive())
+  cat("WARNING: You are encouraged to run the server in batch; you can do so by executing 'Rscript server.r' from a terminal.\n\n")
+
 library(pbdZMQ)
 ctxt <- init.context()
 socket <- init.socket(ctxt, "ZMQ_REP")
