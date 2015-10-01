7c1a4dbd0760db233d66fabc983b7e2311977a5f
wrathematics <wrathematics@gmail.com>
2015-10-01 15:32:59 -0400

    added czmq example

--- inst/examples/reqrep/server.r
+++ inst/examples/reqrep/server.r
@@ -2,24 +2,26 @@ if (interactive())
   cat("WARNING: You are encouraged to run the server in batch; you can do so by executing 'Rscript server.r' from a terminal.\n\n")
 
 library(pbdZMQ)
-ctxt <- init.context()
-socket <- init.socket(ctxt, "ZMQ_REP")
-bind.socket(socket, "tcp://*:5555")
+ctxt <- zmq.ctx.new()
+socket <- zmq.socket(ctxt, .pbdZMQEnv$ZMQ.ST$REP)
+zmq.bind(socket, "tcp://*:5555")
 
 
 while(TRUE)
 {
   cat("Client command:  ")
-  msg <- receive.socket(socket)
+  msg <- zmq.msg.recv(socket)
   cat(msg, "\n")
-
+  
   if (msg == "EXIT")
     break
   
   result <- eval(parse(text=msg))
-
-  send.socket(socket, result)
+  
+  zmq.msg.send(result, socket)
 }
 
-send.socket(socket, "shutting down!")
+zmq.msg.send("shutting down!", socket)
 
+zmq.close(socket)
+zmq.ctx.destroy(ctxt)
