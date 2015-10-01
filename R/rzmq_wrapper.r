a3b2f4110dfeaf6dc228bfc6dc13881f24da56a6
wrathematics <wrathematics@gmail.com>
2015-10-01 09:06:32 -0400

    added rzmq-compatible binding manual entries

--- R/rzmq_wrapper.r
+++ R/rzmq_wrapper.r
@@ -1,9 +1,24 @@
 #' All Wrapper Functions for rzmq
 #' 
-#' Wrapper functions for backwards compatibility with rzmq.
+#' Wrapper functions for backwards compatibility with rzmq.  See vignette
+#' for examples.
 #' 
 #' @details
-#' TODO
+#' \code{send.socket()}/\code{receive.socket()} send/receive messages over
+#' a socket.  These are simple wrappers around \code{zmq.msg.send()} and
+#' \code{zmq.msg.receive()}, respectively.
+#' 
+#' \code{init.context()} creates a new ZeroMQ context.  A useful wrapper
+#' around \code{zmq.ctx.new()} which handles freeing memory for you, i.e.
+#' \code{zmq.ctx.destroy()} will automatically be called for you.
+#' 
+#' \code{init.socket()} creates a ZeroMQ socket; serves as a high-level
+#' binding for \code{zmq.socket()}, including handling freeing memory
+#' automatically.  See also \code{.pbdZMQEnv$ZMQ.ST}.
+#' 
+#' \code{bind.socket()}:  see \code{zmq.bind()}.
+#' 
+#' \code{connect.socket()}:  see \code{zmq.connect()}
 #' 
 #' @param socket
 #' A ZMQ socket.
@@ -19,19 +34,20 @@
 #' @param context
 #' A ZMQ context.
 #' @param socket.type
-#' The type of ZMQ socket.
+#' The type of ZMQ socket as a string, of the form "ZMQ_type".  Valid 'type'
+#' values are PAIR, PUB, SUB, REQ, REP, DEALER, PULL, PUSH, XPUB, XSUB, and
+#' STERAM.
 #' @param address
 #' A valid address.  See details.
 #' 
-#' @return
-#' TODO
-#' 
 #' @author Wei-Chen Chen \email{wccsnow@@gmail.com}.
+#' 
 #' @references ZeroMQ/4.1.0 API Reference:
 #' \url{http://api.zeromq.org/4-1:_start}
 #' 
 #' Programming with Big Data in R Website: \url{http://r-pbd.org/}
-#' @keywords internal
+#' 
+#' @keywords rzmq
 #' @rdname xx_rzmq_wrapper
 #' @name Wrapper Functions for rzmq
 NULL
@@ -70,7 +86,7 @@ init.context <- function(){
   try.zmq.ctx.destroy <- function(ctx){
     try(zmq.ctx.destroy(ctx), silent = TRUE)
   }
-
+  
   ctx <- zmq.ctx.new()  
   reg.finalizer(ctx, try.zmq.ctx.destroy, onexit = TRUE)
   ctx
@@ -84,7 +100,7 @@ init.socket <- function(context, socket.type){
   try.zmqt.close <- function(socket){
     try(zmq.close(socket), silent = TRUE)
   }
-
+  
   socket.type <- sub(".*_", "", socket.type)
   id <- which(names(.pbdZMQEnv$ZMQ.ST) == socket.type)
   if(length(id) != 1){
@@ -92,7 +108,7 @@ init.socket <- function(context, socket.type){
   } else{
     socket.type <- .pbdZMQEnv$ZMQ.ST[[id]]
   }
-
+  
   socket <- zmq.socket(context, type = socket.type)
   reg.finalizer(socket, try.zmqt.close, onexit = TRUE)
   socket
