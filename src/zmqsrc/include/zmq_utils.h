44ff2f4af08fe8e977913be52c14a94870b6ed6a
wccsnow <wccsnow@gmail.com>
2018-01-07 15:15:33 -0500

    Disable pragma ... gagin

--- src/zmqsrc/include/zmq_utils.h
+++ src/zmqsrc/include/zmq_utils.h
@@ -36,13 +36,13 @@
 
 #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__) || defined(_MSC_VER)
 #if defined(__GNUC__) || defined(__GNUG__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic warning "-Wcpp"
-#pragma GCC diagnostic ignored "-Werror"
-#pragma GCC diagnostic ignored "-Wall"
+//WCC #pragma GCC diagnostic push
+//WCC #pragma GCC diagnostic warning "-Wcpp"
+//WCC #pragma GCC diagnostic ignored "-Werror"
+//WCC #pragma GCC diagnostic ignored "-Wall"
 #endif
-#pragma message("Warning: zmq_utils.h is deprecated. All its functionality is provided by zmq.h.")
+//WCC #pragma message("Warning: zmq_utils.h is deprecated. All its functionality is provided by zmq.h.")
 #if defined(__GNUC__) || defined(__GNUG__)
-#pragma GCC diagnostic pop
+//WCC #pragma GCC diagnostic pop
 #endif
 #endif
